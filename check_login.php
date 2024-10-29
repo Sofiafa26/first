b809679bd8eadf33f3707c4a6038e7f0ccd00431
kanyapach <kanyapachw2004@gmail.com>
2024-10-29 10:37:10 +0700

    เปลี่ยนขนาดตัวอักษร

--- check_login.php
+++ check_login.php
@@ -21,7 +21,7 @@ if($result->num_rows > 0){
 
         }
         else{
-            echo "<h1 style='color: red;'>Password is incorrect</h1>";
+            echo "<h2 style='color: red;'>Password is incorrect</h2>";
             //cooldown time
             header("refresh: 2; url=login.php");
         }
